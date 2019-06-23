0732958f88879686802c1a81b41a9958c5349e87
unknown <lainaliam5599@gmail.com>
2019-06-23 15:13:19 +0800

    Added add/edit motor features

---
+++ src/server/models/Motor.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const MotorSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  motorModel: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  weight: {
+    type: String,
+    required: true
+  },
+  height: {
+    type: String,
+    required: true
+  },
+  width: {
+    type: String,
+    required: true
+  },
+  length: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Motor = mongoose.model("motor", MotorSchema);
--- src/server/routes/api/motors.js
+++ src/server/routes/api/motors.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Motor = require("../../models/Motor");
 const authorized = require("../../middleware/authorized");
 
 //@ROUTE          here
@@ -7,6 +8,62 @@ const authorized = require("../../middleware/authorized");
 //@ACCESS         here
 router.get("/", authorized, (req, res) => {
   res.send("motors");
+});
+
+//@ROUTE          localhost:5000/api/motors/create
+//@DESCRIPTION    create a motor
+//@ACCESS         private
+router.post("/create", authorized, async (req, res) => {
+  const { motorModel, image, type, weight, height, length, width } = req.body;
+  const { id } = req.user;
+
+  try {
+    let motor = await Motor.findOne({ motorModel });
+
+    if (motor) {
+      return status(400).json({ message: "Exists" });
+    }
+
+    newMotor = await new Motor({
+      motorModel,
+      image,
+      type,
+      weight,
+      height,
+      length,
+      width,
+      user: id
+    });
+
+    await newMotor.save();
+
+    res.json(newMotor);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+//@ROUTE          localhost:5000/api/motors/edit/:id
+//@DESCRIPTION    edit a motor info
+//@ACCESS         private
+router.put("/edit/:id", authorized, async (req, res) => {
+  const { motorModel, image, type, weight, height, length, width } = req.body;
+  const { id } = req.params;
+
+  try {
+    await Motor.findById(id).updateOne({
+      motorModel,
+      image,
+      type,
+      weight,
+      height,
+      length,
+      width
+    });
+    res.json({ message: "Updated" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
 module.exports = router;
